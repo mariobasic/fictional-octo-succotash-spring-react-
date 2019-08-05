62ee63f37fd4ca4eff4a80674691eee97ad700c5
mariobasic <mbasicpalkovic@execom.eu>
2019-08-05 12:21:19 +0200

    branch 3 commit

--- src/main/java/domain/Project.java
+++ src/main/java/domain/Project.java
@@ -1,14 +1,18 @@
 package domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jdk.jfr.DataAmount;
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Entity
@@ -18,13 +22,28 @@ public class Project {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @NotBlank(message = "Project name is required")
   private String projectName;
+
+  @NotBlank(message = "Project Identifier is required")
+  @Size(min=4, max=5, message = "Please use 4 or 5 characters")
+  @Column(updatable = false, unique = true)
   private String projectIdentifier;
+
+  @NotBlank(message = "Project description is required")
   private String description;
+
+  @JsonFormat(pattern = "yyyy-mm-dd")
   private LocalDate startDate;
+
+  @JsonFormat(pattern = "yyyy-mm-dd")
   private LocalDate endDate;
 
+  @JsonFormat(pattern = "yyyy-mm-dd")
   private LocalDate createdAt;
+
+  @JsonFormat(pattern = "yyyy-mm-dd")
   private LocalDate updatedAt;
 
   @PrePersist
