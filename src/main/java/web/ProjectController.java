23de6b40da5cdd25c4e7e42ccfdcd23cb24a74da
mariobasic <mbasicpalkovic@execom.eu>
2019-08-05 12:41:40 +0200

    error handling in controller

--- src/main/java/web/ProjectController.java
+++ src/main/java/web/ProjectController.java
@@ -2,13 +2,21 @@ package web;
 
 import domain.Project;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import services.ProjectService;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/project")
@@ -22,8 +30,20 @@ public class ProjectController {
   }
 
   @PostMapping()
-  public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+  public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+
+    if (result.hasErrors()) {
+
+      Map<String, String> errorMap = result.getFieldErrors()
+                                           .stream()
+                                           .collect(Collectors.toMap(FieldError::getField,
+                                               DefaultMessageSourceResolvable::getDefaultMessage, (s, s2) -> s2));
+
+      return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+    }
+
     Project project1 = projectService.saveOrUpdate(project);
     return new ResponseEntity<>(project1, HttpStatus.CREATED);
   }
+
 }
